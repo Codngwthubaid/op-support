29cb915cc0a1885d92751c3c1706b9db36e04321
Codngwthubaid <codngwthubaid@gmail.com>
2026-02-03 10:10:00 +0530

    init the next application, update the global.ts file (custom css code)

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "OP Support - AI powered customer care chatbot builder",
+  description: "OP Support is an AI powered customer care chatbot builder that integrate with your existing doc's, website links, manual text data to provide accurate and instant responses to customer queries. And you can integrate it with multiple platforms with a single script.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
